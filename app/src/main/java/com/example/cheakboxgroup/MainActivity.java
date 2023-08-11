2ff0dcdf1e162e7bfaf1cb99534004dd44798bd7
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-08-11 13:57:34 +0400

    Initial commit

---
+++ app/src/main/java/com/example/cheakboxgroup/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.cheakboxgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox pizza,coffe,burger;
+    Button buttonOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+    public void addListenerOnButtonClick(){
+
+        pizza=findViewById(R.id.checkBox);
+        coffe=findViewById(R.id.checkBox2);
+        burger=findViewById(R.id.checkBox3);
+        buttonOrder=findViewById(R.id.button);
+
+
+        buttonOrder.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int totalamount=0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+                if(pizza.isChecked()){
+                    result.append("\nPizza 100Rs");
+                    totalamount+=100;
+                }
+                if(coffe.isChecked()){
+                    result.append("\nCoffe 50Rs");
+                    totalamount+=50;
+                }
+                if(burger.isChecked()){
+                    result.append("\nBurger 120Rs");
+                    totalamount+=120;
+                }
+                result.append("\nTotal: "+totalamount+"Rs");
+                Toast.makeText(getApplicationContext(),result.toString(),Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+
+}
